055fa3e39a8b9198f325c74a2414302f9218bc01
DickersonAJ <adickerson10@stuy.edu>
2020-12-07 15:59:45 -0500

    added the skeleton

---
+++ Range.java
@@ -0,0 +1,11 @@
+public class Range implements IntegerSequence{
+  private int start,end,current;
+  public Range(int start,  int end){  }
+  public void reset(){  }
+  public int length(){  }
+  public boolean hasNext(){  }
+
+  //@throws NoSuchElementException
+  public int next(){    }
+
+}
