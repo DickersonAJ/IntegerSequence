91c9395dab898df73cfd7da6c61ebe26d1ae77f6
DickersonAJ <>
2020-12-08 11:48:24 -0500

    added and tested the first constructor with starting methods

--- ArraySequence.java
+++ ArraySequence.java
@@ -1,9 +1,31 @@
+import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
   private int currentIndex;
   private int []data;
 
   public ArraySequence(int [] other){
-    
+    data = other;
+    currentIndex = 0;
+  }
+
+  public void reset(){
+    currentIndex = 0;
+  }
+
+  public int length(){
+    return data.length;
+  }
+
+  public boolean hasNext(){
+    return currentIndex < length();
+  }
+
+  public int next(){
+    if (hasNext() == false){
+      throw new NoSuchElementException("the element " + currentIndex + " is not in this ArraySequence");
+    }
+    currentIndex+=1;
+    return data[currentIndex - 1];
   }
 
 }
--- tester.java
+++ tester.java
@@ -12,13 +12,22 @@ public class tester{
     r.reset();
     System.out.println(r.length());
     int i = 0;
-    while(i < 10){
+    /*while(i < 10){
       System.out.print(r.next());
       if( r.hasNext() ){
          System.out.print( ", " );
       }
       i+=1;
+    }*/
+
+    int[]nums = {1,3,5,0,-1,3,9};
+    IntegerSequence as = new ArraySequence(nums);
+
+    System.out.println("ArraySequence(array):");
+    while(as.hasNext()){
+      System.out.print(as.next()+", ");
     }
+
   }
 
 }
